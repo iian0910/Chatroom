5ce277390351593d3ff208c52cdd51b69a1b3741
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2024-03-05 16:09:16 +0800

    Updates

---
+++ components/Chat.vue
@@ -0,0 +1,49 @@
+<template>
+  <Info :username="username"/>
+  <MessageContainer
+    :messages="messages"
+    :username="username"
+  />
+  <MessageInput :username="username"/>
+</template>
+
+<script setup>
+import Info from './Info.vue';
+import MessageContainer from './MessageContainer.vue';
+import MessageInput from './MessageInput.vue';
+import dayjs from 'dayjs'
+import { onMounted, ref } from 'vue';
+import { onValue } from 'firebase/database';
+import { realtimeRef } from '../firebase.config';
+
+const props = defineProps({
+  username: {
+    type: String,
+    required: true
+  }
+})
+
+let loginTimestamp = 0
+let messages = ref([])
+
+onMounted(() => {
+  loginTimestamp = dayjs().unix()
+  onValue(realtimeRef, (snapshot) => {
+    if (snapshot.exists()) {
+      let size = 1
+      snapshot.forEach(item => {
+        if (snapshot.size === size && item.key !== null && Number(item.key) >= loginTimestamp) {
+          messages.value.push(item.val())
+        }
+        size++
+      })
+    }
+  }, (error) => {
+    console.log(error)
+  })
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/Chatroom.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container class="pa-0 w-25 chatroom-container">
+    <Login
+      v-if="step === 1"
+      @next="nextStep"
+    />
+    <Chat
+      v-if="step === 2"
+      :message="message"
+      :username="username"
+    />
+  </v-container>
+</template>
+
+<script setup>
+  import Login from './Login.vue'
+  import Chat from './Chat.vue'
+  import { ref } from 'vue'
+
+  const step = ref(1)
+  const username = ref('')
+
+  const nextStep = (name) => {
+    if (name !== '') {
+      username.value = name
+      step.value = 2
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.chatroom-container{
+    margin: 0 auto;
+    border: 1px solid #F4F4F4;
+}
+</style>
+
